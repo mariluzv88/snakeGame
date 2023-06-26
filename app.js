a049dc83cb68c7670bffb1e23f1e393914be723c
Mariluz Vargas <mariluzv88@gmail.com>
2023-06-26 11:09:44 -0400

    Finish wire Framing

---
+++ app.js
@@ -0,0 +1,47 @@
+// ---------------Game Loop-------------------------
+// hudscore: score counter 
+// check score limit for 2nd round ++speed
+// check score limit for 3rd round ++speed
+// check win based on  3rd round score amount for player 1
+// check for gameover if player 2 wins 
+// player 1 or player2 hit wall win/lose condition
+//check is  players enter food postion (unshift,pop) snake object
+
+// ------------player inputs----------
+// playr one up/down/left/right input direction
+// player 1 snake object
+    // player 1 snake head array ...(unshift) based on input direction
+    // player 1 snake tail ...array(pop) based on input direction
+// player 1 snake body speed per sec
+    // x, y postions
+// player 1 movement inputs based on x , y postions
+// spawn postion x,y
+
+
+// player 2 snake object
+     // player 2 snake head array ...(unshift) based on input direction
+    // player 2 snake tail ...array(pop) based on input direction
+// player 2 snake body speed per sec
+    // x, y postions
+// player 2 movement inputs based on x , y postions
+// random spawn position
+
+
+// -------------food pellets--------------
+// food object
+// 2 pellets spawn random x, y postion
+// pop/unshift upon snake object entering x,y,postion
+
+
+// ---------stretch goals------------------
+// player2 based on random direction until  food is at radius  location
+    //math.random directon (if food is x num of boxes away go towards food)
+
+// rounds have different themes
+
+// when score gets close to round limit blink animation and music for intensity
+
+
+
+
+
